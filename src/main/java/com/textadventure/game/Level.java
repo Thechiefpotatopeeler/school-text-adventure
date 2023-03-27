37b64a549e957754eb9a076d51c8c9427bc5b003
thechiefpotatopeeler <thechiefpotatopeeler@gmail.com>
2023-03-28 12:41:57 +1300

    Added json.simple library

--- src/main/java/com/textadventure/game/Level.java
+++
@@ -1,4 +0,0 @@
-package com.textadventure.game;
-
-public class Level {
-}
---
+++ src/main/java/com/textadventure/handlers/WorldLoader.java
@@ -0,0 +1,11 @@
+package com.textadventure.handlers;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+public class WorldLoader {
+    public void WorldLoader(){
+
+    }
+}
