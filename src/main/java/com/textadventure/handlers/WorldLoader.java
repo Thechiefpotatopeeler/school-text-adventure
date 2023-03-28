fb6f3d04526be71ddbf99a3ee5734968e047759e
thechiefpotatopeeler <thechiefpotatopeeler@gmail.com>
2023-03-28 15:16:48 +1300

    Continued world json file input system

--- src/main/java/com/textadventure/handlers/WorldLoader.java
+++ src/main/java/com/textadventure/handlers/WorldLoader.java
@@ -14,16 +14,19 @@ import java.io.FileReader;
 import java.io.Reader;
 
 public class WorldLoader {
-    public WorldLoader(String worldFile) throws IOException, ParseException {
+
+    private ArrayList<Room> rooms = new ArrayList<Room>();
+    public WorldLoader(String worldFile) throws IOException, ParseException, FileNotFoundException {
         JSONParser parser = new JSONParser();
         Reader reader = new FileReader(worldFile);
         JSONObject worldJSON = (JSONObject) parser.parse(reader);
-
+        JSONArray roomList = (JSONArray) worldJSON.get("rooms");
+        for(int i = 0; i < roomList.size(); i++){
+            JSONObject currentRoom = (JSONObject) roomList.get(i);
+            rooms.add(new Room((Integer) currentRoom.get("id"),(String) currentRoom.get("name"),(String) currentRoom.get("description")));
+        }
     }
-
-    public Room loadRoomFromJSON(int roomNumber){
-        Room room;
-
-        return null;
+    public ArrayList<Room> getRooms(){
+        return rooms;
     }
 }
