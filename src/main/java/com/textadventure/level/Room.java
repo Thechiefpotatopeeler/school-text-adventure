bb4f3abf0801b2ac5ea6a1708eb48802fd71104a
thechiefpotatopeeler <thechiefpotatopeeler@gmail.com>
2023-03-28 15:16:48 +1300

    Continued world json file input system

--- src/main/java/com/textadventure/level/Room.java
+++ src/main/java/com/textadventure/level/Room.java
@@ -1,4 +1,13 @@
 package com.textadventure.level;
 
 public class Room {
+
+    private final int id;
+    private final String name;
+    private final String description;
+    public Room(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
--- src/main/java/com/textadventure/level/World.java
+++ src/main/java/com/textadventure/level/World.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class World {
 
+    public ArrayList<Room> rooms;
     public World(){
-
     }
 }
